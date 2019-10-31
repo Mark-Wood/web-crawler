e722562f2a4cca5196e98c23e198cc573fc541ad
Mark Wood <markwood23@btinternet.com>
2019-10-31 20:05:06 +0000

    Add command-line argument handling

--- main/main.go
+++ main/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
-	Crawl("https://www.civica.com", 2)
+	var err error
+	maxDepth := -1
+
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		fmt.Println(errors.New("Usage: web-crawler <url> [max-depth]"))
+		os.Exit(1)
+	}
+
+	if len(os.Args) == 3 {
+		maxDepth, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println(errors.New("Usage: web-crawler <url> [max-depth]"))
+			os.Exit(1)
+		}
+	}
+	page, err := Crawl(os.Args[1], maxDepth)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	Print(page)
 }
